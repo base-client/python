d0c859b7e5f615b9fe1c9784b2dc7598481011c0
heaven-chp <62167253+heaven-chp@users.noreply.github.com>
2024-02-05 05:32:22 +0900

    [#1] [socket] develop

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # base-client-python
+
+## Features
+ - socket
+
+<br/>
+
+## Requirements
+ - `pip install -r requirements.txt`
+
+<br/>
+
+## Run
+ - `python ./base_client/socket/__main__.py --address 0.0.0.0 --port 10000 --timeout 3`
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="base-client",
+    version="0.0.1",
+    description="base client",
+    url="https://github.com/heaven-chp/base-client-python",
+    long_description="",
+    long_description_content_type="text/markdown",
+    author="heaven-chp",
+    packages=find_packages(exclude=["tests"]),
+    install_requires=[],
+)
