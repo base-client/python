c93840da99c024b6a51db89bcb01e0d9525ebe98
heaven-chp <62167253+heaven-chp@users.noreply.github.com>
2024-03-31 13:36:13 +0900

    [#4] edit setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="base-client",
-    version="0.0.1",
+    version="0.0.2",
     description="base client",
     url="https://github.com/base-client/python",
     long_description="",
